7bef57e92ba942003a596bf01e841d41636368fa
Hugo Pereira <hugoper_cs@yahoo.com>
2026-05-10 09:56:59 -0300

    styles: fix Header media query styles

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,9 +3,18 @@ import styled from 'styled-components';
 export const Container = styled.header`
   display: flex;
   justify-content: space-between;
+  width: 100%;
+  height: 60px;
   align-items: center;
   background-color: transparent;
   color: ${({ theme }) => theme.primary};
-  margin-bottom: 25px;
-  position: fixed;
+  margin-bottom: 40px;
+  top: 0;
+  z-index: 10;
+  padding: 0 20px;
+
+  @media (min-width: 768px) {
+    padding: 0;
+    margin-bottom: 0;
+  }
 `;
