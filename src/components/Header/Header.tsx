28da287c2f3bd415f9dae3ce718affd03a56ad8f
Hugo Pereira <hugoper_cs@yahoo.com>
2026-05-11 14:15:38 -0300

    add: logout button to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,13 @@
 import { ThemeToggleButton } from '../ThemeToggleButton/ThemeToggleButton';
 import { Container } from './styles';
+import { useAuth } from '../../context/AuthContext';
 
 export const Header = () => {
+  const { isAuthenticated, logout } = useAuth();
+
   return (
     <Container>
+      {isAuthenticated && <button onClick={logout}>Logout</button>}
       <ThemeToggleButton />
     </Container>
   );
