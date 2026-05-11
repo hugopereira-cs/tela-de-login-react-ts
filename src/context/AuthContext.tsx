27e91cf9be22e389692b9e3545688919a4224944
Hugo Pereira <hugoper_cs@yahoo.com>
2026-05-11 13:36:38 -0300

    add: AuthContext

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,28 @@
+import { createContext, useState, useContext, ReactNode } from 'react';
+
+/**
+ * Contexto de autenticação para gerenciar o estado de login do usuário.
+ * Fornece funções para login e logout, além de um estado booleano para indicar se o usuário está autenticado.
+ */
+interface AuthContextData {
+  isAuthenticated: boolean;
+  login: () => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = () => setIsAuthenticated(true);
+  const logout = () => setIsAuthenticated(false);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
