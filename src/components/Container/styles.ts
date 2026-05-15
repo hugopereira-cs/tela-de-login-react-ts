460659d67901bbff50072493e86682156d92527d
Hugo Pereira <hugoper_cs@yahoo.com>
2026-05-15 15:08:13 -0300

    styles: fix Container component min-height

--- src/components/Container/styles.ts
+++ src/components/Container/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  min-height: 100vh;
+  min-height: calc(100vh - 60px); // 60px é a altura do header
   display: flex;
   flex-direction: column;
   justify-content: center;
