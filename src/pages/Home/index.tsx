5bb6a0df959c5d7d0d6d1eb247048a3930907743
Hugo Pereira <hugoper_cs@yahoo.com>
2026-05-15 14:04:36 -0300

    feat(home): personalize welcome message with user name or email nickname

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,16 @@
 import { HomeContainer, HomeText, Title } from './styles';
 import { Container } from '../../components/Container/Container';
+import { useAuth } from '../../context/AuthContext';
 
-// TODO: Adicionar uma forma de mostrar o nome da pessoa logada detro do HomeText ou do Title
 export const Home = () => {
+  const { user } = useAuth();
+
+  const userName = user?.user_metadata?.display_name || user?.email?.split('@')[0] || 'Usuário'; // Exibe o nome de usuário ou nick do email (antes da @) ou "Usuário"
+
   return (
     <Container>
       <HomeContainer>
-        <Title>Bem vindo a área de usuário!</Title>
+        <Title>Bem vindo {userName}!</Title>
         <HomeText>
           Esta é a página inicial do usuário, onde você pode acessar suas informações e
           configurações.
